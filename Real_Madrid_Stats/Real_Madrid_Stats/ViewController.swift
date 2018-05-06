ed230938fa572cb01ab6780996a82651c844a5f7
Ivan Prokopenko <isprokop@gmail.com>
2018-05-06 18:53:04 +0300

    Created the main hierarchy of the app

---
+++ Real_Madrid_Stats/Real_Madrid_Stats/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Real_Madrid_Stats
+//
+//  Created by Ivan Prokopenko on 06.05.2018.
+//  Copyright © 2018 Ivan Prokopenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
