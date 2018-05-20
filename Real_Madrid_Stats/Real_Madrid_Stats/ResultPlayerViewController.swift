cb2d1e58dceecdc9c68f80e7c0faeabab14c93dd
Ivan Prokopenko <isprokop@gmail.com>
2018-05-20 20:36:50 +0300

    Changed the hierarchy of the app
    added images of players for future
    created some swift files for future work

---
+++ Real_Madrid_Stats/Real_Madrid_Stats/ResultPlayerViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ResultPlayerViewController.swift
+//  Real_Madrid_Stats
+//
+//  Created by Ivan Prokopenko on 20.05.2018.
+//  Copyright © 2018 Ivan Prokopenko. All rights reserved.
+//
+
+import Foundation
